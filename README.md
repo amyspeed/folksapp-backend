6ac5cb811150a91452048c15df76ade2b0ad1413
Amy <amy.speed.henley@gmail.com>
2019-06-12 04:09:16 -0500

    README

---
+++ README.md
@@ -0,0 +1,9 @@
+# FolksApp | An All-Inclusive App for the People 
+
+Register, log in, view and update your profile... 
+Then get to know all of the folks of folksapp! 
+
+## Front-End
+
+This repo is back-end only.
+See the Front-End Repo [HERE](https://github.com/amyspeed/folksapp-frontend)
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/folksapp';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://amyspeed:Password123@ds335957.mlab.com:35957/folksapp';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-folksapp';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
