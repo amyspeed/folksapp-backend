b31b05074db268f50c0f434e3fd9541c54a6eb95
Amy <amy.speed.henley@gmail.com>
2019-06-13 09:15:16 -0500

    file upload

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const passport = require('passport');
 const cors = require('cors');
+const multer = require('multer');
 
 const { router: usersRouter } = require('./users');
 const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
@@ -31,6 +32,28 @@ app.use('/api/auth', authRouter);
 
 app.get('/api', (req, res) => {
     res.json({ok: true});
+});
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    cb(null, 'public')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' +file.originalname )
+  }
+})
+
+const upload = multer({ storage: storage }).single('file')
+
+app.post('/api/upload',function(req, res) {
+    upload(req, res, function (err) {
+           if (err instanceof multer.MulterError) {
+               return res.status(500).json(err)
+           } else if (err) {
+               return res.status(500).json(err)
+           }
+      return res.status(200).send(req.file)
+    })
 });
 
 app.use('*', function (req, res) {
